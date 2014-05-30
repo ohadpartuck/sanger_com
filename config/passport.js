a4fe1bc309da8bf3a03bf10f947498b6d263921e
ohad partuck <ohadpartuck@gmail.com>
2014-05-31 00:41:32 +0300

    wokring on autocomplete

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,7 @@
 var _ = require('underscore');
 var passport = require('passport');
-//var InstagramStrategy = require('passport-instagram').Strategy;
-//var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
-//var TwitterStrategy = require('passport-twitter').Strategy;
-//var GitHubStrategy = require('passport-github').Strategy;
-//var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-//var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-//var OAuthStrategy = require('passport-oauth').OAuthStrategy; // Tumblr
-//var OAuth2Strategy = require('passport-oauth').OAuth2Strategy; // Venmo, Foursquare
-//var User = require('../models/User');
 var secrets = require('./secrets');
-
-//passport.serializeUser(function(user, done) {
-//  done(null, user.id);
-//});
-//
-//passport.deserializeUser(function(id, done) {
-//  User.findById(id, function(err, user) {
-//    done(err, user);
-//  });
-//});
 
 
 /**
